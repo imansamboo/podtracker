6b51bd2cd82faefb1ed4a26f4fcb20682c1ac497
iman <m.jafari@parspooyesh.com>
2025-11-16 07:30:29 +0330

    add new crd

--- apply_required_role.sh
+++ apply_required_role.sh
@@ -21,4 +21,30 @@ roleRef:
   kind: ClusterRole
   name: podtracker-manager
   apiGroup: rbac.authorization.k8s.io
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: raytracker-role
+  namespace: default
+rules:
+  - apiGroups: ["crd.devops.toolbox"]
+    resources: ["raytrackers"]
+    verbs: ["get", "list", "watch", "create", "update", "patch", "delete"]
+
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: raytracker-rolebinding
+  namespace: default
+subjects:
+  - kind: ServiceAccount
+    name: default
+    namespace: default
+roleRef:
+  kind: Role
+  name: raytracker-role
+  apiGroup: rbac.authorization.k8s.io
+
 EOF
--- cmd/main.go
+++ cmd/main.go
@@ -185,6 +185,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PodTracker")
 		os.Exit(1)
 	}
+	if err := (&controller.RayTrackerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "RayTracker")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
